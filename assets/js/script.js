1db71e63dd9131778c38fcf369f61bfd09cea953
vkfbmzef <monkeylordjunior@gmail.com>
2020-09-20 19:46:20 -0700

    print localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,55 +5,90 @@ var tasks = [];
 var text1 = document.createElement("li");
 var tasksToDoEl = document.querySelector("#hour1")
 
-
-//General click event setup
-
-// $(".saveBtn").on('click', function (event) {}
-
-
-
-
-// Display the current day at the top of the page
+// Display current day and use moment to format
 $("#currentDay").append(moment().format('dddd, MMMM Do'));
 
-// populates textareas with respective tasks from local storage
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-
-    // if local storage is null, recreate the array to hold tasks
-    if (!tasks) {
-        tasks = [];
-    }
-
-    // Loop through our saved tasks and assign text to proper textareas
-    for (i = 0; i < tasks.length; i++) {
-        // Grab the id of each object in tasks array
-        var taskId = tasks[i].identifier;
-        // Find the textarea with the same id
-        var test = $("#" + taskId);
-        // Set the textarea to the saved task text
-        test.val(tasks[i].text);
-    }
-};
-
 // Function to save tasks to local storage
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
-
-// I need to create a function to save the shiz to local storage
-$(".description").on("click", ".saveBtn", function() {
+$(".saveBtn").on('click', function (event) {
     // Grab the text from the task input
-    newTask = $(this).prev();
-    taskId = newTask.attr("id");
-    
-    tasks.push({
-        text: newTask.val().trim(),
-        identifier: taskId
-    });
-
-    saveTasks();
+   var newTask = $(this).prev(".description").val();
+   console.log(newTask)
+   var time = $(this).siblings(".description").attr("id")
+   console.log(time)
+localStorage.setItem(time, newTask);    
 });
+
+// for (var i = 1; i < 10; i++) {
+//     loadTasks("hour" + i);
+//   }
+
+// $( ".inner" ).append( "<p>Test</p>" );
+
+var loadTasks = function(time) {
+        var retrieve = localStorage.getItem(time);
+        console.log(retrieve);
+        // store the value of all time slots in an array in 'tasks'
+        tasks = [retrieve];
+        console.log(tasks);
+        //attempting to print entered content back to page
+        $("#" + time).siblings(".description").val(retrieve);
+
+        //to continue loop and not get stuck by setting that localstorage value to null
+        // if (localStorage == null) {
+        //     localStorage.setItem();
+        //  }
+    }
+    // create new element with jQuery
+    // add localstorage value to the element
+    // append the element to the page
+    // given hour1 use jquery to find the textarea of the item we want to append to
+    // "#" + time will be the jquery selector, will give the element(s) that match the selector
+    // use jquery's relation such as sibling hour1.sibling.textarea 
+    // #hour1.append     // append the element to the page
+
+      
+
+
+// var masterList;
+
+// window.onload = function(){
+//   masterList = JSON.parse(localStorage.getItem('masterList')); //get data from storage
+//   if (masterList !== null) { //if data exist (todos are in storage)
+//     masterList.forEach(function(v){ //append each element into the dom
+//       var task = v;
+//       var entry = document.createElement('li'); //2
+//       var list = document.getElementById('orderedList'); //2
+//       entry.appendChild(document.createTextNode(task)); //2
+//       list.appendChild(entry); //2
+//     })
+//   } else { //if nothing exist in storage, keep todos array empty
+//     masterList = [];
+//   }
+// }
+
+
+
+// populates textareas with respective tasks from local storage
+// var loadTasks = function() {
+
+//     localStorage.getItem();
+//     // tasks = JSON.parse(localStorage.getItem("tasks"));
+
+//     // if local storage is null, recreate the array to hold tasks
+//     if (!tasks) {
+//         tasks = [];
+//     }
+
+//     // Loop through our saved tasks and assign text to proper textareas
+//     for (i = 0; i < tasks.length; i++) {
+//         // Grab the id of each object in tasks array
+//         var taskId = tasks[i].identifier;
+//         // Find the textarea with the same id
+//         var test = $("#" + taskId);
+//         // Set the textarea to the saved task text
+//         test.val(tasks[i].text);
+//     }
+// };
 
 // Function to color-code my tasks
 var colorTask = function() {
@@ -91,5 +126,7 @@ setInterval(function() {
     colorTask();
 }, 60000);
 
-loadTasks();
+for (var i = 1; i < 10; i++) {
+    loadTasks("hour" + i);
+  }
 colorTask();
